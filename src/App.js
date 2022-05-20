9f5580d0797a56b42c859e36b25524f851f2adf5
Jhonatan C Hurtado <jhonatan.cortes@servinformacion.com>
2022-05-20 11:41:42 -0500

    Modificación en App JS

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p> Branch: Main </p>
+        <p> Deploy Branch: Main </p>
       </header>
     </div>
   );

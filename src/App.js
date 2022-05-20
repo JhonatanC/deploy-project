5e8561ea2e193fef7a525d14a88e2df008593d62
Jhonatan C Hurtado <jhonatan.cortes@servinformacion.com>
2022-05-20 16:42:33 -0500

    change title

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p> Reasele Pipeline Develop - Branchs Develop. </p>
+        <p> Branchs Develop. </p>
       </header>
     </div>
   );
